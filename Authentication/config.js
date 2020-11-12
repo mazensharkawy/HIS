6f08f220de44dcac592fd7da314b12dabc320cc6
Mazen Sharkawy <mazenmaher1997@gmail.com>
2020-11-12 18:25:03 +0200

    add db

---
+++ Authentication/config.js
@@ -0,0 +1,7 @@
+
+export default {
+	database :{
+		"test" : "mongodb+srv://root:graduation2020@his.8pqkg.mongodb.net/HIS?retryWrites=true&w=majority",
+        "prod" : "mongodb+srv://blloc_server:pUh-4Sn-Dzh-zut@blloc.any4l.mongodb.net/blloc?retryWrites=true&w=majority"
+	}
+}
