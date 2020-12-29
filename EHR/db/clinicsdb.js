9bbc756e60d6bbe3e9213e502895c54214fd65df
Omar Gamily <omar.e.gamily@gmail.com>
2020-12-30 01:31:54 +0200

    create connection fixed

--- EHR/db/clinicsdb.js
+++ EHR/db/clinicsdb.js
@@ -1,67 +1,40 @@
-const {MongoClient} = require('mongodb');
+import { MongoClient } from "mongodb";
 import config from "../config";
+
 const key = config.database;
 const dbName = "HIS";
 
 var db = null;
-const createConnection =  async (fnc)=>{
-    console.log("in connect");
-//  MongoClient.connect(key,
-//     {
-//       useNewUrlParser: true,
-//     },
-//     (err, database) => {
-//       if (err) return console.log(err);
-//       console.log(database);
-//       db = database.db(dbName);
-//       console.log('in connect',database.db());
-//     //   cb && cb();
-//     }
-//   );
-   MongoClient.connect(key,{useNewUrlParser:true}, async (err,database)=>{
-    if(err){
-        console.log("error connecting: "+ err);
-    }else{
-        console.log("database:  "+database);
-            console.log("database2:  "+database.db(dbName));
-            
-            db = database.db(dbName);
-            await fnc();
-    }
-   });
-//    .then((database)=>{
-
-//             // console.log("database:  "+database);
-//             // console.log("database2:  "+database.db(dbName));
-            
-//             db = database.db(dbName);
-        
-//     }).catch((err)=>{
-//         console.log("error connecting: "+err);
-//     })
+const createConnection = async () => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(
+      key,
+      {
+        useNewUrlParser: true,
+      },
+      (err, database) => {
+        if (err) reject(err);
+        db = database.db(dbName);
+        db.once("open", () => console.log("connected to db"));
+        resolve();
+      }
+    );
+  });
 };
 
-
-    // export const  openDb = async ()=> {
-    //   return MongoClient.connect(key, { useNewUrlParser: true }).then(
-    //     (dbc) => (db = dbc.db(dbName))
-    //   );
-    // }
-
-    export const read= async (table,query)=>{
-        if (!db) await createConnection();
-        return await db.collection(table).find(query).toArray();
-    }
-    /**
-     * return value => Promise
-     */
-    export const insert = async (table,data)=>{
-        console.log('db:',db);
-        if (!db){
-              await createConnection(()=>{ db.collection(table).insertOne(data);});
-             console.log("connection created:" + db );
-            }
-        console.log("inserting",db);
-        // db.collection(table).insertOne(data);
-
-    }
+export const read = async (table, query) => {
+  if (!db) await createConnection();
+  return await db.collection(table).find(query).toArray();
+};
+/**
+ * return value => Promise
+ */
+export const insert = (table, data) => {
+  return new Promise(async (resolve, reject) => {
+    if (!db) await createConnection();
+    db.collection(table).insertOne(data, (err, res) => {
+      if (err) reject("error occured durin insertion");
+      resolve("object inserted");
+    });
+  });
+};
--- EHR/routes/clinics.js
+++ EHR/routes/clinics.js
@@ -1,26 +1,25 @@
 import Router from "express";
-import {insert,openDb} from "../db/clinicsdb";
+import { insert, openDb } from "../db/clinicsdb";
 const router = Router();
 
-const insertClinic = (req,res)=> {
-    const data = {
-        name : "ay7aga",
-        open : "date",
-        close: "date",
-        address : "address"
-    }
-    const table = "Clinic";
-    insert(table,data)
-    .then((iserted)=>{
-        console.log("inserted");
-        res.status(200).send(inserted)
-    })
-    .catch((err)=>{
-        console.log("error");
-       console.log(err);
-        res.status(400).send("bad request")
-    });
-}
+const insertClinic = async (req, res) => {
+  const data = {
+    name: "ay7aga",
+    open: "date",
+    close: "date",
+    address: "address",
+  };
+  const table = "Clinic";
 
-router.get('/DDl',insertClinic);
+  insert(table, data)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send();
+    });
+};
+
+router.get("/DDl", insertClinic);
 export default router;
