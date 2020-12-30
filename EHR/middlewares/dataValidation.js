e350f6507e7c28c06e2b759791683ac68a0e6860
Omar Gamily <omar.e.gamily@gmail.com>
2020-12-30 22:25:31 +0200

    timings insert , timing data validation middleware

--- EHR/middlewares/dataValidation.js
+++ EHR/middlewares/dataValidation.js
@@ -1,4 +1,5 @@
 import _ from "lodash";
+
 export const clinicValidation = (req, res, next) => {
   let data = req.body;
 
@@ -16,6 +17,30 @@ export const clinicValidation = (req, res, next) => {
   if (typeof data["name"] != "string" || typeof data["address"] != "string")
     if (!errors["name missing"] && !errors["address missing"])
       errors["name and address should be strings"] = 1;
+
+  if (_.keys(errors) == 0) next();
+  else {
+    let err = "Data invalid: " + _.keys(errors).toString();
+    res.status(400).send(err);
+  }
+};
+
+export const timingValidation = (req, res, next) => {
+  let data = req.body;
+  let errors = {};
+
+  if (!data["doctor"]) errors["doctor _id missing"] = 1;
+  if (!data["clinic"]) errors["clinic _id missing"] = 1;
+  if (!data["start"]) errors["start time missing"] = 1;
+  if (!data["end"]) errors["end time missing"] = 1;
+
+  if (typeof data["start"] != "object" || typeof data["end"] != "object")
+    if (!errors["start time missing"] && !errors["end time missing"])
+      errors["start and end should be 'Date' format"] = 1;
+
+  if (typeof data["doctor"] != "string" || typeof data["clinic"] != "string")
+    if (!errors["doctor _id missing"] && !errors["clinic _id missing"])
+      errors["doctor and clinic should be _id strings"] = 1;
 
   if (_.keys(errors) == 0) next();
   else {
--- EHR/routes/clinics.js
+++ EHR/routes/clinics.js
@@ -1,6 +1,9 @@
 import Router from "express";
 import { insert } from "../db/clinicsdb";
-import { clinicValidation } from "../middlewares/dataValidation";
+import {
+  clinicValidation,
+  timingValidation,
+} from "../middlewares/dataValidation";
 const router = Router();
 
 const insertClinic = async (req, res) => {
@@ -14,6 +17,19 @@ const insertClinic = async (req, res) => {
       res.status(400).send();
     });
 };
+const insertTiming = async (req, res) => {
+  let data = req.body;
+  insert("Timing", data)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).send();
+    });
+};
 
-router.post("/insert", clinicValidation, insertClinic);
+router.post("/insert-clinic", clinicValidation, insertClinic);
+router.post("/insert-timing", timingValidation, insertTiming);
+
 export default router;
