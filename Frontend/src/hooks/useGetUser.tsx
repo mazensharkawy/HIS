82056fb7fddbfaf2a0c9fefff8f5f81d210dcaae
Ahmed Ibrahim Ali <ahmed.ibr2020@gmail.com>
2021-03-16 16:58:22 +0200

    Public data

--- Frontend/src/hooks/useGetUser.tsx
+++ Frontend/src/hooks/useGetUser.tsx
@@ -3,11 +3,11 @@ import { IPatient } from '../interfaces/patient';
 export function useGetUser(): IPatient {
   return {
     age: 25,
-    name: 'Liam',
-    lastName: 'Jouns',
+    name: 'Khaled',
+    lastName: 'Younis',
     number: '0126596578',
     gender: 'male',
-    address: '71 Pilgrim Avenue Chevy Chase, MD 20815',
+    address: '7170 شارع بورسعيد، الإبراهيمية بحري وسيدي جا، قسم باب شرقي، الإسكندرية',
     status: 'approved',
     lastVisit: '18 Dec 2020',
     img: `${window.origin}/content/male.png`
